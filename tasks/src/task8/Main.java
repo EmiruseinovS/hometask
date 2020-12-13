e0d80c464888c43a665b9b65324c70e7659ad0e6
Aziz <selimemiruseinov@gmail.com>
2020-12-13 21:04:47 +0300

    solved tasks 8

---
+++ tasks/src/task8/Main.java
@@ -0,0 +1,10 @@
+package task8;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        a = 20; b = 30;
+        System.out.println("Среднее арифметичекое чисел " + a + " и " + b + " равно " + (a + b)/2);
+    }
+}
+
