d4e2d2f57014ec1049674f9650ab0b2beefc83d2
Aziz <selimemiruseinov@gmail.com>
2020-12-13 20:57:09 +0300

    solved tasks 7

---
+++ tasks/src/task7/Main.java
@@ -0,0 +1,12 @@
+package task7;
+
+public class Main {
+    public static void main(String[] args) {
+        double P, L, S, R;
+        R = 30;
+        P = 3.14;
+        L = 2 * P * R;
+        S = P * (R * R);
+        System.out.println("Радиус окружности равен " + R + ", длина окружности равна " + L + ", а площадь круга составляет " + S);
+    }
+}
