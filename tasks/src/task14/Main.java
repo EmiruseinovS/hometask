3dba0c207d19c5595d6aa376602c28392bcdc1dd
Aziz <selimemiruseinov@gmail.com>
2020-12-14 14:40:52 +0300

    solved tasks 14

---
+++ tasks/src/task14/Main.java
@@ -0,0 +1,12 @@
+package task14;
+
+public class Main {
+    public static void main(String[] args) {
+        double L, R, S;
+        R = 20;
+        L = 2 * 3.14 * R;
+        S = 3.14 * (R * R);
+        System.out.println("Длина окружности равна " + L);
+        System.out.println("Площадь окружности равна " + S);
+    }
+}
