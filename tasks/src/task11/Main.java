6137c6db05fec7fae96995d3a2fc7597222fdf4e
Aziz <selimemiruseinov@gmail.com>
2020-12-14 14:21:49 +0300

    solved tasks 11

---
+++ tasks/src/task11/Main.java
@@ -0,0 +1,12 @@
+package task11;
+
+public class Main {
+    public static void main(String[] args) {
+        double a, b;
+        a = 20; b = 10;
+        System.out.println("Сумма модулей переменных равна " + Math.abs(a + b));
+        System.out.println("Разность модулей переменных равна " + Math.abs(a - b));
+        System.out.println("Произведение модулей переменных равно " + Math.abs(a * b));
+        System.out.println("Частное модулей переменных равно " + Math.abs(a / b));
+    }
+}
