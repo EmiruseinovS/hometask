a5b34a414026bb701047a7167424561758b9ee85
Aziz <selimemiruseinov@gmail.com>
2020-12-14 15:07:27 +0300

    corrected task 11

--- tasks/src/task11/Main.java
+++ tasks/src/task11/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         double a, b;
         a = 20; b = 10;
-        System.out.println("Сумма модулей переменных равна " + Math.abs(a + b));
-        System.out.println("Разность модулей переменных равна " + Math.abs(a - b));
-        System.out.println("Произведение модулей переменных равно " + Math.abs(a * b));
-        System.out.println("Частное модулей переменных равно " + Math.abs(a / b));
+        System.out.println("Сумма модулей переменных равна " + Math.abs(a) + Math.abs(b));
+        System.out.println("Разность модулей переменных равна " + Math.abs(a) - Math.abs(b));
+        System.out.println("Произведение модулей переменных равно " + Math.abs(a) * Math.abs(b));
+        System.out.println("Частное модулей переменных равно " + Math.abs(a) / Math.abs(b));
     }
 }
