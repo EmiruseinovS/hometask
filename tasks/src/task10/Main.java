47bca14c5662cbdf42601831cf48516fcc880591
Aziz <selimemiruseinov@gmail.com>
2020-12-14 13:55:08 +0300

    solved tasks 10

---
+++ tasks/src/task10/Main.java
@@ -0,0 +1,13 @@
+package task10;
+
+public class Main {
+    public static void main(String[] args) {
+        int x, y;
+        x = 3;
+        y = 2;
+        System.out.println("Сумма переменных равна " + ((x * x) + (y * y)));
+        System.out.println("Разность переменных равна " + ((x * x) - (y * y)));
+        System.out.println("Произведение переменных равно " + ((x * x) * (y * y)));
+        System.out.println("Частное чисел равно " + ((x * x) / (y * y)));
+    }
+}
