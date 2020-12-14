1cfa65ffa1d356ffe628f1a0c952eb497b502305
Aziz <selimemiruseinov@gmail.com>
2020-12-14 14:48:21 +0300

    solved tasks 15

---
+++ tasks/src/task15/Main.java
@@ -0,0 +1,15 @@
+package task15;
+
+public class Main {
+    public static void main(String[] args) {
+        double D, L, S, R;
+        R = 100;
+        L = 2 * 3.14 * R;
+        S = 3.14 * (R * R);
+        D = R * 2;
+        System.out.println("Радиус окружности равен " + R);
+        System.out.println("Площадь окружности равна " + S);
+        System.out.println("Длина окружности равна " + L);
+        System.out.println("Диаметр окружности равен " + D);
+    }
+}
