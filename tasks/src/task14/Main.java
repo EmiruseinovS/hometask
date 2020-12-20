0c5f46c58c787bf6a473e27480b04ec06ce7e013
Aziz <selimemiruseinov@gmail.com>
2020-12-20 19:46:09 +0300

    corrected task 14

--- tasks/src/task14/Main.java
+++ tasks/src/task14/Main.java
@@ -3,8 +3,8 @@ package task14;
 public class Main {
     public static void main(String[] args) {
         double L, R, S;
-        R = 20;
-        L = 2 * 3.14 * R;
+        L = 20;
+        R = L/(2 * 3.14);
         S = 3.14 * (R * R);
         System.out.println("Длина окружности равна " + L);
         System.out.println("Площадь окружности равна " + S);
