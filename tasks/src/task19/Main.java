44d37ba4ace45832a6366bf088a01a0b125627b9
Aziz <selimemiruseinov@gmail.com>
2020-12-25 17:58:08 +0300

    Solved task 19(if)

--- tasks/src/task19/Main.java
+++ tasks/src/task19/Main.java
@@ -5,24 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner task = new Scanner(System.in);
-        System.out.println("Введите координаты первой вершины: ");
-        System.out.println("х1 равен: ");
-        int x1 = task.nextInt();
-        System.out.println("у1 равен: ");
-        int y1 = task.nextInt();
-        int x2 = x1 - x1 - x1;
-        System.out.println("х2 равен: " + x2);
-        int y2 = y1 - y1 - y1;
-        System.out.println("y2 равен: " + y2);
-        int a, b, c, d; // a верхняя сторона, с нижняя. в и d боковые
-        a = Math.abs(x1) * 2;
-        c = a;
-        b = y1 * 2;
-        d = b;
-        System.out.println("Стороны а и с равны: " + a + "; Стороны в и d равны: " + b);
-        int P, S;
-        P = (a + c) + (b + d);
-        S = a * b;
-        System.out.println("Периметр прямоугольника равен: " + P + "; Площадь прямоугольника равна: " + S);
+        System.out.println("Введите первую переменную: ");
+        int num1 = task.nextInt();
+        System.out.println("Введите вторую переменную: ");
+        int num2 = task.nextInt();
+        System.out.println("Введите третью переменную: ");
+        int num3 = task.nextInt();
+        if(num1>= 0 && num2 >= 0 && num3 >=0)
+            System.out.println("Количество положительных переменных равно 3");
+        else if(num1 >= 0 && num2 >= 0 || num2 >= 0 && num3 >= 0 || num1 >= 0 && num3 >= 0)
+            System.out.println("Количество положительных переменных равно 2");
+        else if(num1 >= 0 || num2 >= 0 || num3 >= 0)
+            System.out.println("Количество положительных переменных равно 1");
     }
 }
