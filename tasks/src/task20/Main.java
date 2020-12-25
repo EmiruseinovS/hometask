fb8742c1f374386c2ff222e85bf312efb500c1ec
Aziz <selimemiruseinov@gmail.com>
2020-12-25 18:02:05 +0300

    Solved task 20(if)

--- tasks/src/task20/Main.java
+++ tasks/src/task20/Main.java
@@ -1,20 +1,29 @@
 package task20;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner task = new Scanner(System.in);
-        System.out.println("Введите координаты x1: ");
-        double x1 = task.nextDouble();
-        System.out.println("Введите координаты y1: ");
-        double y1 = task.nextDouble();
-        System.out.println("Введите координаты x2: ");
-        double x2 = task.nextDouble();
-        System.out.println("Введите координаты y2: ");
-        double y2 = task.nextDouble();
-        double distance;
-            distance = Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
-        System.out.println("Дистанция между клетками: " + distance);
+        System.out.println("Введите первую переменную: ");
+        int num1 = task.nextInt();
+        System.out.println("Введите вторую переменную: ");
+        int num2 = task.nextInt();
+        System.out.println("Введите третью переменную: ");
+        int num3 = task.nextInt();
+        if(num1>= 0 && num2 >= 0 && num3 >=0)
+            System.out.println("Количество положительных переменных равно 3");
+        else if(num1 >= 0 && num2 >= 0 || num2 >= 0 && num3 >= 0 || num1 >= 0 && num3 >= 0)
+            System.out.println("Количество положительных переменных равно 2");
+        else if(num1 >= 0 || num2 >= 0 || num3 >= 0)
+            System.out.println("Количество положительных переменных равно 1");
+        if(num1 <= 0 && num2 <= 0 && num3 <=0)
+            System.out.println("Количество отрицательных переменных равно 3");
+        else if(num1 <= 0 && num2 <= 0 || num2 <= 0 && num3 <= 0 || num1 <= 0 && num3 <= 0)
+            System.out.println("Количество отрицательных переменных равно 2");
+        else if(num1 <= 0 || num2 <= 0 || num3 <= 0)
+            System.out.println("Количество отрицательных переменных равно 1");
     }
 }
