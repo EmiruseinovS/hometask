b277c5d709f992aff60df4bd6dfdd1e9e163c476
Aziz <selimemiruseinov@gmail.com>
2020-12-28 19:56:19 +0300

    solved task 30(if)

--- tasks/src/task30/Main.java
+++ tasks/src/task30/Main.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner task = new Scanner(System.in);
-        System.out.println(": ");
+        System.out.println("Введите первое число: ");
         int A = task.nextInt();
-        System.out.println(": ");
+        System.out.println("Введите второе число: ");
         int B = task.nextInt();
-        System.out.println(": ");
+        System.out.println("Введите третье число: ");
         int C = task.nextInt();
+        if(A >= B && C >= B)
+            System.out.println("Сумма двух наибольших чисел равна: " + (A + C));
+        else if(B >= A && C >= A)
+            System.out.println("Сумма двух наибольших чисел равна: " + (B + C));
+        else if(A >= C && B >= C)
+            System.out.println("Сумма двух наибольших чисел равна: " + (A + B));
+
     }
 }
