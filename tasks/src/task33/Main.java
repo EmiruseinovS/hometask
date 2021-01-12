7ca795c9604927b48807e84d0a1655fd4c300a88
Aziz <selimemiruseinov@gmail.com>
2021-01-12 19:09:49 +0300

    solved task 33(for)

---
+++ tasks/src/task33/Main.java
@@ -0,0 +1,19 @@
+package task33;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+        System.out.println("Введите число А: ");
+        int A = task.nextInt();
+        System.out.println("Введите число В: ");
+        int B = task.nextInt();
+        if(A > B)
+            for(int i = A - 1;i > B;i--)
+                System.out.println(i);
+        else if(A < B)
+            for(int i = B - 1;i > A;i--)
+                System.out.println(i);
+    }
+}
