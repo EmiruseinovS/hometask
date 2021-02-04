1dd5262beaeaea19df395de96695f40680d06d86
Aziz <selimemiruseinov@gmail.com>
2021-02-04 11:06:47 +0300

    solved task 44(for)

---
+++ tasks/src/task44/Main.java
@@ -0,0 +1,16 @@
+package task44;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+        System.out.println(":");
+        int n = task.nextInt();
+        long res = n * n;
+        for (int i = 1; i < res; i+=2) {
+            System.out.println(i);
+        }
+        System.out.println(res);
+    }
+}
