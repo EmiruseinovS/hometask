0663785f2ef72eb8a2177a415882fcf257885640
Aziz <selimemiruseinov@gmail.com>
2021-02-04 11:07:22 +0300

    solved task 44(for)

--- tasks/src/task44/Main.java
+++ tasks/src/task44/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner task = new Scanner(System.in);
-        System.out.println(":");
+        System.out.println("Введите n:");
         int n = task.nextInt();
         long res = n * n;
         for (int i = 1; i < res; i+=2) {
