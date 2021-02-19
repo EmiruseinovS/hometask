5968c310b81cf011b6d139b0a1e41ca990e4677a
Aziz <selimemiruseinov@gmail.com>
2021-02-19 21:17:22 +0300

    solved task 63(array)

---
+++ tasks/src/task63/Main.java
@@ -0,0 +1,20 @@
+package task63;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+        System.out.println("n: ");
+        int n = task.nextInt();
+        System.out.println("a: ");
+        int a = task.nextInt();
+        System.out.println("d: ");
+        int d = task.nextInt();
+        int[] array = new int[n];
+        for (int i = 1; i < n; i++) {
+            array[i] = a + d * i;
+            System.out.println("array[" + i + "]=" + array[i]);
+        }
+    }
+}
