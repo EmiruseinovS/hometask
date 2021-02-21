276c6fa006e724470db0423e8f466f797d87d8df
Aziz <selimemiruseinov@gmail.com>
2021-02-21 20:52:53 +0300

    resolved task 32(for)

--- tasks/src/task32/Main.java
+++ tasks/src/task32/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner task = new Scanner(System.in);
         System.out.println("Введите число А: ");
-        int A = task.nextInt();
+        int a = task.nextInt();
         System.out.println("Введите число В: ");
-        int B = task.nextInt();
-        if(A>B)
-            for(int i = B + 1;i < A;i++)
-                System.out.println(i);
-        else if(A<B)
-            for(int i = A + 1;i < B;i++)
-                System.out.println(i);
+        int b = task.nextInt();
+        int score = 0;
+        for (int i = a; i  <= b; i++) {
+            score++;
+            System.out.println(i);
+        }
+        System.out.println("Количество чисел: " + score);
     }
 }
